84eca7c42da0d6917109c049e6c8a8dd7c060086
asphodel-student <m.komarov2@g.nsu.ru>
2023-12-09 22:50:43 +0700

    Added launch file

--- core/controller_node/controller_node/robot_controller.py
+++ core/controller_node/controller_node/robot_controller.py
@@ -2,6 +2,7 @@ import rclpy
 from rclpy.node import Node
 
 from messages.msg import TrafficSign
+from messages.msg import TrafficLight
 from messages.msg import ControlMsg
 
 class Controller(Node):
@@ -9,9 +10,11 @@ class Controller(Node):
         super().__init__('robot_controller')
 
         self.traffic_sign_detector_subscription = self.create_subscription(TrafficSign, '/sign_topic', self.get_traffic_sign, 10)
+        self.traffic_light_checker_subscription = self.create_subscription(TrafficLight, '/traffic_light', self.get_traffic_sign, 10)
 
         self.pid_control = self.create_publisher(ControlMsg, '/pid_control', 1)
         self.sign_detector_control = self.create_publisher(ControlMsg, '/sign_controller', 1)
+        self.tl_detector_control = self.create_publisher(ControlMsg, '/tl_control', 1)
 
         self.is_allowed_to_move = False
         self.current_traffic_sign = -1
@@ -29,8 +32,19 @@ class Controller(Node):
         print(self.current_traffic_sign)
 
 
-    def get_traffic_light_color(self):
-        pass
+    def get_traffic_light_color(self, msg):
+        if msg.is_green == True:
+            self.is_allowed_to_move = True
+
+            # Turn off the light checker node
+            self.shutdown_light_checker()
+
+            # Turn off the pid
+            self.turn_on_pid()
+
+            # Turn off the sign detector
+            self.turn_on_sign_detector()
+
 
     def shutdown_pid(self):
         """
@@ -65,6 +79,14 @@ class Controller(Node):
         msg = ControlMsg()
         msg.mode = True
         self.sign_detector_control.publish(msg)
+
+    def shutdown_light_checker(self):
+        """
+        Позволяет выключить ноду распознавания сигналов светофора
+        """
+        msg = ControlMsg()
+        msg.mode = False
+        self.tl_detector_control.publish(msg)
 
 
 def main(args=None):
---
+++ core/controller_node/launch/launch_me.launch.py
@@ -0,0 +1,34 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+       
+        Node(
+            package='controller_node', 
+            executable='controller',
+            name='controller'),
+        
+        Node(
+            package='check_traffic_light', 
+            executable='traffic_light_checker',
+            name='tl_checker'),
+
+        Node(
+            package='line_following', 
+            executable='pid_test',
+            name='pid'),
+
+        Node(
+            package='sign_detector', 
+            executable='sign_detection',
+            name='sign_detection'),
+    ])
