a04b8ab9fb104c32e25b41906623c167ba27c3db
asphodel-student <m.komarov2@g.nsu.ru>
2023-12-09 23:44:30 +0700

    Method of detection traffic light's mode was simplified

--- camera/check_traffic_light/check_traffic_light/check_traffic_light.py
+++ camera/check_traffic_light/check_traffic_light/check_traffic_light.py
@@ -17,44 +17,37 @@ class TrafficLightDetector(Node):
         super().__init__('traffic_light_detector')
 
         self.subscribtion = self.create_subscription(Image, '/color/image', self.detect_traffic_light_color_callback, 10)
-        self.control_subscribtion = self.create_subscription(ControlMsg, '/control_tl', self.shutdown_callback, 10)
+        # self.control_subscribtion = self.create_subscription(ControlMsg, '/control_tl', self.shutdown_callback, 10)
+        
         self.publisher = self.create_publisher(TrafficLight, '/traffic_light', 1)
 
-        self.is_green_light = False
-        self.future = None
+        self._bridge = CvBridge()
 
     def detect_traffic_light_color_callback(self, image_msg):
+        """
+        Функция, проверяющая светофор на наличие разрешющего сигнала.
+        Реализована самым простейшим образом.
+        При обнаружении зеленого цвета, отправляет сообщение в главную ноду и уничтожается
+        """
+        frame = self._bridge.imgmsg_to_cv2(image_msg, "bgr8")
 
-        frame = self.bridge.imgmsg_to_cv2(image_msg, "mono8")
-        blurred = cv2.GaussianBlur(frame, (21, 21), 0)
-        edges = cv2.Canny(blurred, 25, 30)
+        target_color = np.array([2, 110, 2])
+        mask = np.all(frame == target_color, axis=-1)
 
-        contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-        for contour in contours:
-            if len(contour) > 6:
-                ellipse = cv2.fitEllipse(contour)
-                major_axis, minor_axis = max(ellipse[1]), min(ellipse[1])
-                if minor_axis == 0:
-                    continue
-                
-                center_pixel = tuple(map(int, ellipse[0]))
-                color = frame[center_pixel[1], center_pixel[0]]
-
-                is_green = all(abs(a - b) < 5 for a, b in zip(color, [0, 117, 0]))
-                if is_green:
-                    self.is_green_light = True
-                    msg = TrafficLight()
-                    msg.is_green = True
-                    self.publisher.publish(msg)
-
+        if np.any(mask == True):
+            self.get_logger().info('Green light!!!!')
+            self.is_green_light = True
+            msg = TrafficLight()
+            msg.is_green = True
+            self.publisher.publish(msg)
+            self.destroy_node()
 
     def shutdown_callback(self, msg):
         state = msg.mode
 
         if state == False:
-            self.node.get_logger().info("Shutting down...")
-            self.node.destroy_node()
+            self.get_logger().info("Shutting down...")
+            self.destroy_node()
             rclpy.shutdown()
 
 
