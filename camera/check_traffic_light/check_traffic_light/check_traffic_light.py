3446855035819bb29090bccf52077bc2c059434c
asphodel-student <m.komarov2@g.nsu.ru>
2023-12-09 22:51:25 +0700

    Added subscription to control topic

--- camera/check_traffic_light/check_traffic_light/check_traffic_light.py
+++ camera/check_traffic_light/check_traffic_light/check_traffic_light.py
@@ -9,12 +9,16 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 
+from messages.msg import ControlMsg
+from messages.msg import TrafficLight
+
 class TrafficLightDetector(Node):
     def __init__(self):
         super().__init__('traffic_light_detector')
 
         self.subscribtion = self.create_subscription(Image, '/color/image', self.detect_traffic_light_color_callback, 10)
-        # self.publisher = self.create_publisher(Image, '/color/image', self.detect_traffic_light_color_callback, 10)
+        self.control_subscribtion = self.create_subscription(ControlMsg, '/control_tl', self.shutdown_callback, 10)
+        self.publisher = self.create_publisher(TrafficLight, '/traffic_light', 1)
 
         self.is_green_light = False
         self.future = None
@@ -40,12 +44,29 @@ class TrafficLightDetector(Node):
                 is_green = all(abs(a - b) < 5 for a, b in zip(color, [0, 117, 0]))
                 if is_green:
                     self.is_green_light = True
-                    self.future.set_result(None)
+                    msg = TrafficLight()
+                    msg.is_green = True
+                    self.publisher.publish(msg)
+
 
     def shutdown_callback(self, msg):
-        state = msg.data
+        state = msg.mode
 
-        if state == 'shutdown':
+        if state == False:
             self.node.get_logger().info("Shutting down...")
             self.node.destroy_node()
             rclpy.shutdown()
+
+
+def main():
+    rclpy.init()
+
+    node = TrafficLightDetector()
+
+    rclpy.spin(node)
+
+    node.destroy_node()
+
+    rclpy.shutdown()
+
+main()
