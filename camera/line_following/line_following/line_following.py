Fix some bugs with signs detection

camera/line_following/line_following/line_following.py
@@ -21,12 +21,14 @@ class LineFollower(Node):
         self.frame = None
         self.gray = None
         self.dst = None
-        self.prevpt1 = np.array([150, 40])
-        self.prevpt2 = np.array([560, 40])
+        self.prevpt1 = np.array([150, 60])
+        self.prevpt2 = np.array([560, 60])
         self.error = 0
 
         # This node works when this flag is True
         self.mode = False
+
+        self.is_stopped = False
 
     def subs_callback(self, msg):
         if self.mode == False:
@@ -88,21 +90,11 @@ class LineFollower(Node):
         self.prevpt2 = np.array(cpt2)
 
         fpt = ((cpt1[0] + cpt2[0]) / 2, (cpt1[1] + cpt2[1]) / 2 + self.gray.shape[0] // 3 * 2)
-        cv2.cvtColor(self.dst, cv2.COLOR_GRAY2BGR)
-
-        # cv2.circle(self.frame, (int(fpt[0]), int(fpt[1])), 2, (0, 0, 255), 2)
-        # cv2.circle(self.dst, (int(cpt1[0]), int(cpt1[1])), 2, (0, 0, 255), 2)
-        # cv2.circle(self.dst, (int(cpt2[0]), int(cpt2[1])), 2, (255, 0, 0), 2)
 
         cv2.circle(self.dst, (int(self.prevpt1[0]), int(self.prevpt1[1])), 2, 122, 2)
         cv2.circle(self.dst, (int(self.prevpt2[0]), int(self.prevpt2[1])), 2, 122, 2)
 
         self.error = self.dst.shape[1] // 2 - fpt[0]
-
-        # if self.error < 0:
-        #     self.error = -self.error
-
-        # self.get_logger().info(str(self.error))
 
         cv2.imshow("camera", self.gray)
         cv2.imshow("gray", self.dst)
@@ -111,14 +103,23 @@ class LineFollower(Node):
     def update_callback(self):
         cmd_vel = Twist()
 
-        if self.mode:
+        if self.mode == False:
+            cmd_vel.linear.x = 0.0
+            cmd_vel.angular.z = 0.0
+
+            if self.is_stopped == False:
+                self.cmd_vel_pub.publish(cmd_vel)
+                self.is_stopped = True
+        else:
             cmd_vel.linear.x = 0.2
             cmd_vel.angular.z = (self.error * 90.0 / 400) / 16
-
             self.cmd_vel_pub.publish(cmd_vel)
 
     def check_mode_callback(self, msg):
         self.mode = msg.mode
+
+        if self.mode == True:
+            self.is_stopped = False
 
 
 def main(args=None):

camera/sign_detector/sign_detector/sign_detector.py
@@ -27,7 +27,11 @@ class SignDetector(Node):
 
         self._bridge = CvBridge()
         self._sift = cv2.SIFT_create()
-        self._flann = cv2.BFMatcher()
+
+        FLANN_INDEX_KDTREE = 0
+        index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+        search_params = dict(checks = 50)
+        self._flann = cv2.FlannBasedMatcher(index_params, search_params)
 
         package_share_directory = get_package_share_directory('sign_detector')
         dir_path = os.path.join(package_share_directory, 'data/')
@@ -42,13 +46,17 @@ class SignDetector(Node):
             if image is None:
                 continue
             kp, des = self._sift.detectAndCompute(image, None)
-            self._kp.append(kp)
-            self._des.append(des)
+            self._kp.append(copy.copy(kp))
+            self._des.append(copy.copy(des))
+
+        # image = cv2.imread(dir_path + 'turn_right.png', cv2.IMREAD_GRAYSCALE)
+        # self._kp, self._des = self._sift.detectAndCompute(image, None)
 
         self.sign = -1
         self.mode = True
 
         self.subscriber = self.create_subscription(Image, '/color/image', self.detect_traffic_sign_callback, 10)
+        self.pub = self.create_publisher(Image, '/signs_image', 1)
         self.sign_publisher = self.create_publisher(TrafficSign, '/sign_topic', 1)
 
         # Control topic
@@ -65,7 +73,7 @@ class SignDetector(Node):
         if self.mode == False:
             return
         
-        MIN_MATCH_COUNT = 60
+        MIN_MATCH_COUNT = 20
 
         image = self._bridge.imgmsg_to_cv2(image_data, 'mono8')
         kp1, des1 = self._sift.detectAndCompute(image, None)
@@ -77,14 +85,26 @@ class SignDetector(Node):
         for i, match in enumerate(matches):
             match_points = []
             for m, n in match:
-                if m.distance < 0.7 * n.distance:
+                if m.distance < 0.55 * n.distance:
                     match_points.append(m)
 
             if len(match_points) > MIN_MATCH_COUNT:
-                confidence.append((i, len(match_points)))
+                confidence.append((i, len(match_points), copy.copy(match_points)))
 
         if len(confidence) != 0:
-            index = max(confidence, key=lambda x: x[1])[0]
+            index, length, data = max(confidence, key=lambda x: x[1])
+
+            self.get_logger().info('POINTS: {}'.format(length))
+
+            src_pts = np.float32([ kp1[m.queryIdx].pt for m in data ]).reshape(-1,1,2)
+            dst_pts = np.float32([ self._kp[index][m.trainIdx].pt for m in data ]).reshape(-1,1,2)
+
+            # M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+            # matchesMask_tunnel = mask.ravel().tolist()
+
+            #mse = self.fnCalcMSE(src_pts, dst_pts)
+            #self.get_logger().info('{}'.format(mse))
+            #if mse < 100000:
             old = copy.copy(self.sign)
             self.sign = self.set_current_sign(index)
 
@@ -94,10 +114,63 @@ class SignDetector(Node):
                 msg = TrafficSign()
                 msg.trafic_sign = self.sign
                 self.sign_publisher.publish(msg)
-               
+
+            # draw_params_tunnel = dict(matchColor = (255,0,0), 
+            #                 singlePointColor = None,
+            #                 matchesMask = matchesMask_tunnel, 
+            #                 flags = 2)
+
+            # final_tunnel = cv2.drawMatches(image,kp1,self._signs_images[index], self._kp[index], data, None, **draw_params_tunnel)
+            # self.pub.publish(self._bridge.cv2_to_imgmsg(final_tunnel, "bgr8"))
+
+        # matches = self._flann.knnMatch(des1, self._des, k=2)
+
+        # match_points = []
+        # for m, n in matches:
+        #     if m.distance < 0.7 * n.distance:
+        #         match_points.append(m)
+
+        # if len(match_points) > MIN_MATCH_COUNT:
+        #     src_pts = np.float32([ kp1[m.queryIdx].pt for m in match_points ]).reshape(-1,1,2)
+        #     dst_pts = np.float32([ self._kp[m.trainIdx].pt for m in match_points ]).reshape(-1,1,2)
+
+        #     # M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+        #     # matchesMask_tunnel = mask.ravel().tolist()
+
+        #     mse = self.fnCalcMSE(src_pts, dst_pts)
+
+        #     self.get_logger().info('POINTS: {}'.format(len(match_points)))
+        #     self.get_logger().info('{}'.format(mse))
+
+        #     if mse < 100000:
+        #         old = copy.copy(self.sign)
+        #         self.sign = self.set_current_sign(5)
+
+        #         if old != self.sign:
+        #             self.get_logger().info(str(self.sign))
+
+        #             msg = TrafficSign()
+        #             msg.trafic_sign = self.sign
+        #             self.sign_publisher.publish(msg)
+
+        # draw_params_tunnel = dict(matchColor = (255,0,0), 
+        #                 singlePointColor = None,
+        #                 matchesMask = matchesMask_tunnel, 
+        #                 flags = 2)
+
+        # final_tunnel = cv2.drawMatches(image,kp1,self._signs_images[5], self._kp[5], match_points, None, **draw_params_tunnel)
+        # self.pub.publish(self._bridge.cv2_to_imgmsg(final_tunnel, "bgr8"))
+            
             
     def set_current_sign(self, index: int) -> str:
         return Signs[index]
+    
+    def fnCalcMSE(self, arr1, arr2):
+        squared_diff = (arr1 - arr2) ** 2
+        sum = np.sum(squared_diff)
+        num_all = arr1.shape[0] * arr1.shape[1] #cv_image_input and 2 should have same shape
+        err = sum / num_all
+        return err
     
 
 def main():